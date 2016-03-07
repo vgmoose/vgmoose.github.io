af1d257a70bebf03f06eb6d069c34d8152747d42
Ricky Ayoub <vgmoose@gmail.com>
2016-03-07 01:37:29 -0500

    hash loading for search

--- js/search.js
+++ js/search.js
@@ -1,13 +1,22 @@
 function doSearch() 
 {
     // get all terms
-    var terms = $("#search_input").val().split(" ");
+    var terms = $("#search_input").val().toLowerCase();
+    
+    terms = terms.replace(/[?!-.:/]/g, " ").split(" ");;
+    
+    // set terms to hash
+    location.hash = terms.join(":");
     
     var results = {};
     
     // go through each term and find target pages
     for (var x in terms)
     {
+        // ignore empty string and space
+        if (x == "" || x == " ")
+            continue;
+        
         var term = terms[x];
         if (term in vocab)
         {
@@ -47,4 +56,14 @@ $(function() {
         doSearch();
     });
     
+    // if a hash is set, fill input text and search
+    if (location.hash != "" && location.haah != "#")
+    {
+        var terms = location.hash.split(":");
+        if (terms[0][0] == "#")
+            terms[0] = terms[0].substring(1);
+        
+        $("#search_input").val(terms.join(" "));
+        $("#go_search").click();
+    }
 });
